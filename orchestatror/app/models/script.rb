d98a7a3668c59433da4b365f936f4d6ce02ff658
zerbinidamata <rafael@aeroli.to>
2021-04-11 17:59:48 -0300

    Script uri for shared rules

--- orchestatror/app/models/script.rb
+++ orchestatror/app/models/script.rb
@@ -1,4 +1,6 @@
 class Script < ApplicationRecord
   has_and_belongs_to_many :action
   has_and_belongs_to_many :test_case
+  # This is supposed to be used for shared rules via IPFS
+  has_one_attached :script_uri
 end
